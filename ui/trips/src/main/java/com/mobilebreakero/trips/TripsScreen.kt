184f51ec7079194285f0268e473f5d3610559fe0
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2023-10-16 22:00:33 +0300

    trips_design_v1

--- ui/trips/src/main/java/com/mobilebreakero/trips/TripsScreen.kt
+++ ui/trips/src/main/java/com/mobilebreakero/trips/TripsScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun TripsScreen(){
 
